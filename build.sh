9462534b2fa1c3fbfa32babb7933d552fab65cf3
Jefferson5286 <jeffersonlima5286@gmail.com>
2024-04-14 17:47:02 -0300

    Atualizado build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-gunicorn TaskManage.wsgi.application
