262a3c9ec599371ac54a1caeaa74396db2386677
Jefferson5286 <jeffersonlima5286@gmail.com>
2024-04-15 08:27:57 -0300

    Revert "Merda"
    This reverts commit 6994638d61948e511c9b0eb85e79a09854e21407.
    
    bash: linha 1: v: comando não encontrado

--- build.sh
+++ build.sh
@@ -2,19 +2,8 @@
 # Exit on error
 set -o errexit
 
-echo "install requeriments"
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-echo "Start script.py"
-python script.py
-
-echo "Migrate"
-#  Database migrate
-python manage.py makemigrations
-python manage.py migrate
-
-echo "collect static"
 # Convert static asset files
 python manage.py collectstatic --no-input
-
